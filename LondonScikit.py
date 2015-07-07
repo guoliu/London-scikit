87e63f8f737f4abd33f4350afbee377e9e6cdbf9
Guo Liu <liuguomailbox@gmail.com>
2015-07-06 22:42:52 -0700

    Initi

---
+++ LondonScikit.py
@@ -0,0 +1,32 @@
+print(__doc__)
+'''london-scikit competions'''
+'''@author Guo Liu'''
+
+import numpy as np
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import GridSearchCV
+
+# load data
+x_train = np.loadtxt(open("train.csv","rb"), delimiter=",", skiprows=0)
+y_train = np.loadtxt(open("trainLabels.csv","rb"), delimiter=",", skiprows=0)
+x_test = np.loadtxt(open("test.csv","rb"), delimiter=",", skiprows=0)
+
+# set parameters to be tuned
+tuned_parameters = {'C': 10.**np.arange(-5,5)}
+
+# conduct exhaustive gridsearch
+clf = GridSearchCV(LinearSVC(), tuned_parameters, cv=5)
+clf.fit(x_train, y_train)
+
+# print report
+for params, mean_score, scores in clf.grid_scores_:
+    print("%0.3f (+/-%0.03f) for %r" % (mean_score, scores.std() * 2, params))
+
+# make predictions
+predictions = clf.predict(x_test)
+out = np.vstack((np.arange(len(predictions)).astype(int)+1, predictions.astype(int))).T
+
+# wirte out data
+np.savetxt("LinearSVMresult.csv", out, header="Id,Solution", fmt="%d", comments="", delimiter=",")
